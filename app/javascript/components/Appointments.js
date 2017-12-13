cb19d482106759b3a4f4a6a52305e01928008a87
Suyesh Bhandari <suyeshb@gmail.com>
2017-12-13 18:26:19 -0500

    Using webpacker

---
+++ app/javascript/components/Appointments.js
@@ -0,0 +1,9 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Appointments extends React.Component {
+  render () {
+    return <h1>CalReact</h1>;
+  }
+}
+
+export default Appointments
