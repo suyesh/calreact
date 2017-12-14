3aef62b0ed9cd18213595f3220b0508f45b0779f
Suyesh Bhandari <suyeshb@gmail.com>
2017-12-14 16:40:03 -0500

    Config master key

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Calreact
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.require_master_key  = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
